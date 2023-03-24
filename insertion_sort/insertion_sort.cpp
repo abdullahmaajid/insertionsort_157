a8369975dbed43ad4d73239557f50c3a9c4d0206
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-03-24 10:33:21 +0700

    membuat inputan jumlah element array, memanggil variable inputan n, membuat kondisi n tidak lebih dari 20, dan menampilkan pesan

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -5,3 +5,17 @@ int arr[20];	// Membuat Array dengan panjang data 20
 int n;			// Membuat Variable
 
 void input() {	// Procedure Input
+	while (true)
+	{
+		cout << "Masukkan Jumlah Data pada Array : ";	// Membuat Inputan jumlah element Array
+		cin >> n;										// Memanggil varible inputan n
+
+		if (n <= 20) {									// Membuat kondisi n tidak lebih dari 20
+			break;
+		}
+		else
+		{
+			cout << "\n Array yang anda masukkan maksimal 20 Elemen. \n"; // Menampilkan pesan jika data lebih dari 20
+		}
+	}
+
