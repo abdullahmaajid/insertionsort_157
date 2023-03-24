163bb7900dddf9844af650fbf71b246aedf7a932
abdullahmaajid <abdullah.maajid.ft22@mail.umy.ac.id>
2023-03-24 10:58:37 +0700

    looping nilai k dimulai dari 0 hingga n-1

--- insertion_sort/insertion_sort.cpp
+++ insertion_sort/insertion_sort.cpp
@@ -51,5 +51,7 @@ void insertionsort() {	// Procedure Insertionsort
 		arr[j + 1] = temp;			// 5. simpan nilai temp ke dalam arr [j+1]
 
 		cout << "\nPass " << i << ": "; // Output ke layar
+		for (int k = 0; k < n; k++) {	// Looping nilai k dimulai dari 0 hingga n-1
+
 }
 
